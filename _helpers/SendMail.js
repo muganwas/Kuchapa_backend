145bdec02e41f71e3b5c6926adaa422475dec510
Muganwa <stevenmuganwa@gmail.com>
2020-03-27 17:22:07 +0300

    remove "secure" for nodemailer

--- _helpers/SendMail.js
+++ _helpers/SendMail.js
@@ -20,7 +20,7 @@ module.exports = async (email, subject, message) => {
   let transporter = nodemailer.createTransport({
     host: config.email_host,
     port: config.email_port,
-    secure: process.env.NODE_ENV === 'development' ? false : true, // true for 465, false for other ports
+    //secure: process.env.NODE_ENV === 'development' ? false : true, // true for 465, false for other ports
     requireTLS: true,
     auth: {
       user: config.email_user, // generated ethereal user
