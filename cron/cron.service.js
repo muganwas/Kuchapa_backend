94d86fac8fd6c17ad0ebd08242e15a24ff224b6c
Muganwa <stevenmuganwa@gmail.com>
2020-11-10 08:57:23 +0300

    Update cron.service.js

--- cron/cron.service.js
+++ cron/cron.service.js
@@ -8,54 +8,37 @@ const JobRequest = db.JobRequest;
 const Notification = db.Notification;
 const Employee = db.Employee;
 const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+    "July", "August", "September", "October", "November", "December"
 ];
 const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 const gcm = require("node-gcm");
-
-
 
 module.exports = {
     ChnageReqStatus
 };
 
+async function ChnageReqStatus(param) {
 
-
-
-async function ChnageReqStatus(param){
-    
-    var check =  {};
+    var check = {};
     check['status'] = 'Pending';
     check['chat_status'] = '0';
-   
-    var change =await JobRequest.find(check);
-   if(change.length > 0){
-    
-       for(var i = 0; i< change.length;i++  ){
-   
-     var d = new Date(change[i].createdDate);
-     var p = new Date(d.valueOf() + 4 * 60000).valueOf();
-    var now = new Date().valueOf();    
-        if(p < now){
-            var change_status = await JobRequest.findById(change[i]._id);
-               var update_status = {};
-               update_status['status'] = 'Failed';
+
+    var change = await JobRequest.find(check);
+    if (change.length > 0) {
+
+        for (var i = 0; i < change.length; i++) {
+
+            var d = new Date(change[i].createdDate);
+            var p = new Date(d.valueOf() + 4 * 60000).valueOf();
+            var now = new Date().valueOf();
+            if (p < now) {
+                var change_status = await JobRequest.findById(change[i]._id);
+                var update_status = {};
+                update_status['status'] = 'Failed';
                 Object.assign(change_status, update_status);
-            
-             var change_status_rslt = await change_status.save();
+
+                var change_status_rslt = await change_status.save();
+            }
         }
-    
-            
-       }
-       
-       
-       
-   }
-
-
+    }
 }
-
-
-
-
-
