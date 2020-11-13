d0843cf6758240981e232578e13adadf373edc67
Muganwa <stevenmuganwa@gmail.com>
2020-11-13 23:24:36 +0300

    send image info the way it is from db.

--- main_category/main_category.service.js
+++ main_category/main_category.service.js
@@ -14,24 +14,24 @@ module.exports = {
     _delete
 };
 
-async function respond(socket){
+async function respond(socket) {
 
-global.socket = await socket;
+    global.socket = await socket;
     var data = await Main_Category.find()
-      if(data){
+    if (data) {
         // socket.emit("ADDED_DATA",data);
-      }
+    }
 }
 
 async function getAll() {
     var data = '';
-    if(data = await Main_Category.find()){
-//  global.socket.emit("ADDED_DATA",data);
-    return  {result:true,message:'Service Found',data:data};
-        }else{
-    return  {result:false,message:'Service Not Found'};
+    if (data = await Main_Category.find()) {
+        //  global.socket.emit("ADDED_DATA",data);
+        return { result: true, message: 'Service Found', data: data };
+    } else {
+        return { result: false, message: 'Service Not Found' };
 
-        }
+    }
 }
 
 async function getById(id) {
@@ -40,17 +40,17 @@ async function getById(id) {
 
 async function create(userParam) {
     // validate
-console.log(userParam);
+    console.log(userParam);
 
     const cat = new Main_Category(userParam);
 
 
-     var output = '';
-    if(output = await cat.save()){
-       
-        return {result:true,message:'Add Category Successfull',data:output};
-    }else{
-        return {result:false,message:'Something went wrong'};
+    var output = '';
+    if (output = await cat.save()) {
+
+        return { result: true, message: 'Add Category Successfull', data: output };
+    } else {
+        return { result: false, message: 'Something went wrong' };
     }
 }
 
@@ -58,39 +58,37 @@ async function update(id, userParam) {
     const user = await Main_Category.findById(id);
 
     // validate
-       if(!user){
-        return {result:false,message:"user not found"};
-       }
+    if (!user) {
+        return { result: false, message: "user not found" };
+    }
 
-   
+
     // copy userParam properties to user
     Object.assign(user, userParam);
-  var data ='';
-    if(data = await user.save()){
-        return {result:true,message:"update Successfull",data:data};
+    var data = '';
+    if (data = await user.save()) {
+        return { result: true, message: "update Successfull", data: data };
 
-    }else{
-        return {result:false,message:"Something went wrong"};
-        
+    } else {
+        return { result: false, message: "Something went wrong" };
+
     }
 
 }
 
 async function _delete(id) {
-   
-console.log(id);
-   if(await Main_Category.findById(id)){
-      
-         if(await Main_Category.findByIdAndRemove(id)){
-            return {result:true,message:"Category deleted Successfull"};
-         }else{
-            
-            return {result:false,message:"Something went wrong"};
-         }
 
-   }else{
-        return {result:false,message:"Category not Found"};
-   }
+    console.log(id);
+    if (await Main_Category.findById(id)) {
+
+        if (await Main_Category.findByIdAndRemove(id)) {
+            return { result: true, message: "Category deleted Successfull" };
+        } else {
+
+            return { result: false, message: "Something went wrong" };
+        }
+
+    } else {
+        return { result: false, message: "Category not Found" };
+    }
 }
-
-
--- services/service.service.js
+++ services/service.service.js
@@ -30,95 +30,86 @@ async function authenticate({ username, password }) {
 
 async function getAll() {
     var data = '';
-    if(data = await User.find().select('-hash')){
-   var tempdata = [];
-    for (var i = 0 ; i < data.length ; i++){
-       
-        var main_c = '';
-        var sub_c = '';
-        main_c = await Main_Category.find({_id:data[i].main_category});
-        sub_c = await Sub_Category.find({_id:data[i].sub_category});
-        
-         if(main_c.length > 0 && sub_c.length > 0){
+    if (data = await User.find().select('-hash')) {
+        var tempdata = [];
+        for (var i = 0; i < data.length; i++) {
 
-           data[i].main_category =main_c[0].main_category;
-           data[i].sub_category =sub_c[0].sub_category;
-           data[i].image = config.URL+'api/uploads/services/'+data[i].image;
-           tempdata.push(data[i]);
+            var main_c = '';
+            var sub_c = '';
+            main_c = await Main_Category.find({ _id: data[i].main_category });
+            sub_c = await Sub_Category.find({ _id: data[i].sub_category });
+
+            if (main_c.length > 0 && sub_c.length > 0) {
+                data[i].main_category = main_c[0].main_category;
+                data[i].sub_category = sub_c[0].sub_category;
+                tempdata.push(data[i]);
+            }
+
+
         }
-        
-  
+        return { result: true, message: 'Service Found', data: tempdata };
+    } else {
+        return { result: false, message: 'Service Not Found' };
+
     }
-    return  {result:true,message:'Service Found',data:tempdata};
-        }else{
-    return  {result:false,message:'Service Not Found'};
-
-        }
 }
 
 async function getAllService() {
     var data = '';
-    if(data = await User.find().select('-hash')){
-    for (var i = 0 ; i < data.length ; i++){
-          data[i].image = config.URL+'api/uploads/services/'+data[i].image;   
-    }
-    return  {result:true,message:'Service Found',data:data};
-        }else{
-    return  {result:false,message:'Service Not Found'};
+    if (data = await User.find().select('-hash')) {
+        return { result: true, message: 'Service Found', data: data };
+    } else {
+        return { result: false, message: 'Service Not Found' };
 
-        }
+    }
 }
 
 async function getById(id) {
-    
-    return  await User.findById(id);
+
+    return await User.findById(id);
 }
 
 async function create(userParam) {
     // validate
-
-
     const user = new User(userParam);
-
- 
-     var output = '';
-    if(output = await user.save()){
-        return {result:true,message:'Add Service Successfull',data:output};
-    }else{
-        return {result:false,message:'Something went wrong'};
+    var output = '';
+    if (output = await user.save()) {
+        return { result: true, message: 'Add Service Successfull', data: output };
+    } else {
+        return { result: false, message: 'Something went wrong' };
     }
 }
 
 async function update(id, userParam) {
     const user = await User.findById(id);
 
-     if(!user){
-        return {result:false,message:'Service not found'};
-     }  
+    if (!user) {
+        return { result: false, message: 'Service not found' };
+    }
 
     // copy userParam properties to user
     Object.assign(user, userParam);
-   var output ='';
+    var output = '';
 
-     if(output = await user.save()){
-        return {result:true,message:'Update Service Successfull',data:output};
-    }else{
-        return {result:false,message:'Something went wrong'};
+    if (output = await user.save()) {
+        return { result: true, message: 'Update Service Successfull', data: output };
+    } else {
+        return { result: false, message: 'Something went wrong' };
     }
 }
 
 async function _delete(id) {
-      if(await User.findById(id)){
-      
-         if(await User.findByIdAndRemove(id)){
-            return {result:true,message:"Service deleted Successfull"};
-         }else{
-            
-            return {result:false,message:"Something went wrong"};
-         }
+    if (await User.findById(id)) {
 
-      }else{
-        return {result:false,message:"Service not Found"};
-      }
+        if (await User.findByIdAndRemove(id)) {
+            return { result: true, message: "Service deleted Successfull" };
+        } else {
+
+            return { result: false, message: "Something went wrong" };
+        }
+
+    } else {
+        return { result: false, message: "Service not Found" };
+    }
 
 }
